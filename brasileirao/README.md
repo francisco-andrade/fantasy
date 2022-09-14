1a76945d8d6e21fae9606c2b5c7d112682d47196
Francisco Andrade <francisco.andrade@rci.rogers.com>
2022-09-14 11:55:06 -0400

    Atualiza arquivos do brasileirao

--- brasileirao/README.md
+++ brasileirao/README.md
@@ -7,6 +7,15 @@ Comandos:
 ./sleeper_tool.sh -o matchups -w 1
 ```
 
+## Docker build command
+`docker build -t franciscoandrade/sleeper-brasileirao -f Dockerfile .`
+
+## Docker run example
+`docker run -ti --name sleeper-data -ti --rm --net=host -v /tmp/docker:/tmp/data/ franciscoandrade/sleeper-brasileirao -f ./data/leagues_2022.txt -o matchups -w 1`
+
+## Docker run example with ssh bash
+`docker run -ti --entrypoint bash --name sleeper-data -v /tmp/docker:/tmp/data/ -ti --rm --net=host franciscoandrade/sleeper-brasileirao`
+
 Dependencias:
 ```
 - Python 2.7
--- brasileirao/sleeper_tool.sh
+++ brasileirao/sleeper_tool.sh
@@ -1,22 +1,27 @@
 #!/bin/bash
 
-LEAGUES_FILE=./leagues.txt
-
-if [[ ! -e $LEAGUES_FILE ]]; then
-  echo "Missing leagues file: $LEAGUES_FILE"
-  exit 1
-fi
-
-while getopts ":o:w:" opt; do
+while getopts ":o:w:f:" opt; do
   case $opt in
     o) OPTION="$OPTARG"
     ;;
     w) WEEK="$OPTARG"
     ;;
+    f) LEAGUES_FILE="$OPTARG"
+    ;;
     \?) echo "Invalid option $OPTARG" >&2
     ;;
   esac
 done
+ABSPATH=$(cd "$(dirname "$0")"; pwd)
+OUTPUT_FOLDER=/tmp/data/
+: "${LEAGUES_FILE:=./data/leagues_2022.txt}"
+
+cd $ABSPATH
+
+if [[ ! -e $LEAGUES_FILE ]]; then
+  echo "Missing leagues file: $LEAGUES_FILE"
+  exit 1
+fi
 
 [[ $OPTION == "" ]] && echo "Option (-o) not specified. Valid values are: matchups,users,rosters" && exit 1
 [[ $OPTION == "rosters" ]] || [[ $OPTION == "matchups" ]] && [[ $WEEK == "" ]] && echo "Week (-w) not specified." && exit 1
@@ -25,33 +30,36 @@ echo "Starting script"
 
 while read league; do
   if [[ $OPTION == "users" ]]; then
-      FILENAME=./data/users/${league}_users
+      mkdir -p ${OUTPUT_FOLDER}/data/users
+      FILENAME=${OUTPUT_FOLDER}/data/users/${league}_users
       if [[ ! -e ${FILENAME}.json ]]; then
           echo "Getting League $league users..."
           curl  https://api.sleeper.app/v1/league/${league}/users -s > ${FILENAME}.json
       fi
       echo "Converting League $league users"
-      python ./sleeper_parser.py --file ${FILENAME}.json --option users --league_id $league >> ./data/users.csv
+      python ./sleeper_parser.py --file ${FILENAME}.json --option users --league_id $league >> ${OUTPUT_FOLDER}/data/users.csv
       echo "Users CSV: ./data/users.csv"
   fi
   if [[ $OPTION == "rosters" ]]; then
-      FILENAME=./data/rosters/${league}_rosters_week$WEEK
+      mkdir -p ${OUTPUT_FOLDER}/data/rosters
+      FILENAME=${OUTPUT_FOLDER}/data/rosters/${league}_rosters_week$WEEK
       if [[ ! -e ${FILENAME}.json ]]; then
           echo "Getting League $league rosters..."
           curl  https://api.sleeper.app/v1/league/${league}/rosters -s > ${FILENAME}.json
       fi
       echo "Converting League $league rosters"
-      python ./sleeper_parser.py --file ${FILENAME}.json --option roster-ids --league_id $league >> ./data/rosters_week$WEEK.csv
+      python ./sleeper_parser.py --file ${FILENAME}.json --option roster-ids --league_id $league >> ${OUTPUT_FOLDER}/data/rosters_week$WEEK.csv
       echo "Rosters CSV: ./data/rosters_week$WEEK..csv"
   fi
   if [[ $OPTION == "matchups" ]]; then
-      FILENAME=./data/matchups/${league}_matchups_week${WEEK}
+      mkdir -p ${OUTPUT_FOLDER}/data/matchups
+      FILENAME=${OUTPUT_FOLDER}/data/matchups/${league}_matchups_week${WEEK}
       if [[ ! -e ${FILENAME}.json ]]; then
           echo "Getting League $league matchups..."
           curl  https://api.sleeper.app/v1/league/${league}/matchups/${WEEK} -s > ${FILENAME}.json
       fi
       echo "Converting League $league matchups"
-      python ./sleeper_parser.py --file ${FILENAME}.json --rosters ./data/rosters/${league}_rosters_week$WEEK.json --option matchups --league_id $league --week ${WEEK} >> ./data/matchups_week${WEEK}.csv
+      python ./sleeper_parser.py --file ${FILENAME}.json --rosters ./data/rosters/${league}_rosters_week$WEEK.json --option matchups --league_id $league --week ${WEEK} >> ${OUTPUT_FOLDER}/data/matchups_week${WEEK}.csv
       echo "Matchups CSV: ./data/matchups_week${WEEK}.csv"
     fi
 done < "$LEAGUES_FILE"
