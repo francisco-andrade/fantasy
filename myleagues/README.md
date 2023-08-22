ae9768a4ba14ee08f9d44a242100517dee6ea298
Francisco Andrade <francisco.andrade@rci.rogers.com>
2023-08-22 13:37:37 -0400

    Updates container

--- myleagues/README.md
+++ myleagues/README.md
@@ -5,7 +5,9 @@ Sleeper data to csv container
 `docker build -t franciscoandrade/sleeper-pr -f Dockerfile .`
 
 ## Docker run example
+`docker run -ti --name sleeper-data -ti --rm --net=host franciscoandrade/sleeper-pr -o users -i $LEAGUE_ID`
 `docker run -ti --name sleeper-data -ti --rm --net=host franciscoandrade/sleeper-pr -o power -w 17`
+`docker run -ti --name sleeper-data -ti --rm --net=host franciscoandrade/sleeper-pr -o power -w 15 -i $LEAGUE_ID`
 
 ## Docker run example with ssh bash
 `docker run -ti --entrypoint bash --name sleeper-data -v /tmp/docker:/tmp/data/ -ti --rm --net=host franciscoandrade/sleeper-pr`
--- myleagues/power_rank.sh
+++ myleagues/power_rank.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
 LEAGUES_FILE=./leagues.txt
+ABSPATH=$(cd "$(dirname "$0")"; pwd)
+cd $ABSPATH
 
-if [[ ! -e $LEAGUES_FILE ]]; then
-  echo "Missing leagues file: $LEAGUES_FILE"
-  exit 1
-fi
-
-while getopts ":o:w:" opt; do
+while getopts ":o:w:i:" opt; do
   case $opt in
     o) OPTION="$OPTARG"
     ;;
     w) WEEK="$OPTARG"
+    ;;
+    i) LEAGUE_ID="$OPTARG"
     ;;
     \?) echo "Invalid option $OPTARG" >&2
     ;;
@@ -21,31 +20,46 @@ done
 [[ $OPTION == "" ]] && echo "Option (-o) not specified. Valid values are: users,power" && exit 1
 [[ $OPTION == "power" ]] && [[ $WEEK == "" ]] && echo "Week (-w) not specified." && exit 1
 
-echo "Starting script"
-
-while read league; do
-  if [[ $OPTION == "users" ]]; then
-      FILENAME=./data/${league}_users
-      if [[ ! -e ${FILENAME}.json ]]; then
-          echo "Getting League $league users..."
-          curl  https://api.sleeper.app/v1/league/${league}/users -s > ${FILENAME}.json
-      fi
-      echo "Converting League $league users"
-      python ./sleeper_parser.py --file ${FILENAME}.json --option users --league_id $league >> ./data/users.csv
-      echo "Users CSV: ./data/users.csv"
+print_users(){
+  league=$1
+  FILENAME=./data/${league}_users
+  if [[ ! -e ${FILENAME}.json ]]; then
+      curl  https://api.sleeper.app/v1/league/${league}/users -s > ${FILENAME}.json
   fi
-  if [[ $OPTION == "power" ]]; then
-      FILENAME=./data/${league}_rosters_week$WEEK
-      if [[ ! -e ${FILENAME}.json ]]; then
-          echo "Getting League $league rosters..."
-          curl  https://api.sleeper.app/v1/league/${league}/rosters -s > ${FILENAME}.json
-      fi
-      echo "Generating League $league PR"
-      python ./sleeper_parser.py --file ${FILENAME}.json --option power --league_id $league --week $WEEK >> ./data/power-rank_week$WEEK.csv
-      echo "PR CSV: ./data/power-rank_week$WEEK..csv"
-  fi
-done < "$LEAGUES_FILE"
+  python ./sleeper_parser.py --file ${FILENAME}.json --option users --league_id $league > ./data/users.csv
+  cat ./data/users.csv
+}
 
-echo "Done"
+print_power(){
+  league=$1
+  FILENAME=./data/${league}_rosters_week$WEEK
+  if [[ ! -e ${FILENAME}.json ]]; then
+      curl  https://api.sleeper.app/v1/league/${league}/rosters -s > ${FILENAME}.json
+  fi
+  python ./sleeper_parser.py --file ${FILENAME}.json --option power --league_id $league --week $WEEK > ./data/power-rank_week$WEEK.csv
+  cat ./data/power-rank_week$WEEK.csv
+}
+
+if [[ -n $LEAGUE_ID ]]; then
+    if [[ $OPTION == "users" ]]; then
+      print_users $LEAGUE_ID
+    fi
+    if [[ $OPTION == "power" ]]; then
+      print_power $LEAGUE_ID
+    fi
+else
+  if [[ ! -e $LEAGUES_FILE ]]; then
+    echo "Missing leagues file: $LEAGUES_FILE"
+    exit 1
+  fi
+  while read LEAGUE_ID; do
+    if [[ $OPTION == "users" ]]; then
+      print_users $LEAGUE_ID
+    fi
+    if [[ $OPTION == "power" ]]; then
+      print_power $league
+    fi
+  done < "$LEAGUES_FILE"
+fi
 
 exit 0
