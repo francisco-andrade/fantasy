baff7f8d4c48f53a40faa050457253e74648c0f4
Francisco Andrade <francisco.andrade@rci.rogers.com>
2023-09-13 17:00:01 -0400

    Updates brasileirao files

--- brasileirao/README.md
+++ brasileirao/README.md
@@ -4,14 +4,14 @@ Comandos:
 ./sleeper_tool.sh -o rosters -w 1
 
 # Gerar o csv com os dados de matchups
-./sleeper_tool.sh -o matchups -w 1
+./sleeper_tool.sh -o matchups -w 1 -l 860893886317477888
 ```
 
 ## Docker build command
 `docker build -t franciscoandrade/sleeper-brasileirao -f Dockerfile .`
 
 ## Docker run example
-`docker run -ti --name sleeper-data -ti --rm --net=host -v /tmp/docker:/tmp/data/ franciscoandrade/sleeper-brasileirao -f ./data/leagues_2022.txt -o matchups -w 1`
+`chico@MRMIOMP2441 brasileirao % docker run -ti --name sleeper-data -v /tmp/docker:/tmp/data/ -ti --rm --net=host franciscoandrade/sleeper-brasileirao -o matchups -w 1 -l "860893886317477888 860894291751473152"`
 
 ## Docker run example with ssh bash
 `docker run -ti --entrypoint bash --name sleeper-data -v /tmp/docker:/tmp/data/ -ti --rm --net=host franciscoandrade/sleeper-brasileirao`
--- brasileirao/sleeper_tool.sh
+++ brasileirao/sleeper_tool.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-while getopts ":o:w:f:" opt; do
+while getopts ":o:w:f:l:" opt; do
   case $opt in
     o) OPTION="$OPTARG"
     ;;
@@ -8,12 +8,14 @@ while getopts ":o:w:f:" opt; do
     ;;
     f) LEAGUES_FILE="$OPTARG"
     ;;
+    l) LEAGUES="$OPTARG"
+    ;;
     \?) echo "Invalid option $OPTARG" >&2
     ;;
   esac
 done
 ABSPATH=$(cd "$(dirname "$0")"; pwd)
-OUTPUT_FOLDER=/tmp/data/
+OUTPUT_FOLDER=/tmp
 : "${LEAGUES_FILE:=./data/leagues_2022.txt}"
 
 cd $ABSPATH
@@ -24,46 +26,41 @@ if [[ ! -e $LEAGUES_FILE ]]; then
 fi
 
 [[ $OPTION == "" ]] && echo "Option (-o) not specified. Valid values are: matchups,users,rosters" && exit 1
-[[ $OPTION == "rosters" ]] || [[ $OPTION == "matchups" ]] && [[ $WEEK == "" ]] && echo "Week (-w) not specified." && exit 1
+[[ $OPTION == "matchups" ]] && [[ $WEEK == "" ]] && echo "Week (-w) not specified." && exit 1
 
-echo "Starting script"
-
-while read league; do
-  if [[ $OPTION == "users" ]]; then
-      mkdir -p ${OUTPUT_FOLDER}/data/users
-      FILENAME=${OUTPUT_FOLDER}/data/users/${league}_users
-      if [[ ! -e ${FILENAME}.json ]]; then
-          echo "Getting League $league users..."
-          curl  https://api.sleeper.app/v1/league/${league}/users -s > ${FILENAME}.json
-      fi
-      echo "Converting League $league users"
-      python ./sleeper_parser.py --file ${FILENAME}.json --option users --league_id $league >> ${OUTPUT_FOLDER}/data/users.csv
-      echo "Users CSV: ./data/users.csv"
-  fi
-  if [[ $OPTION == "rosters" ]]; then
-      mkdir -p ${OUTPUT_FOLDER}/data/rosters
-      FILENAME=${OUTPUT_FOLDER}/data/rosters/${league}_rosters_week$WEEK
-      if [[ ! -e ${FILENAME}.json ]]; then
-          echo "Getting League $league rosters..."
-          curl  https://api.sleeper.app/v1/league/${league}/rosters -s > ${FILENAME}.json
-      fi
-      echo "Converting League $league rosters"
-      python ./sleeper_parser.py --file ${FILENAME}.json --option roster-ids --league_id $league >> ${OUTPUT_FOLDER}/data/rosters_week$WEEK.csv
-      echo "Rosters CSV: ./data/rosters_week$WEEK..csv"
-  fi
+process_league() {
   if [[ $OPTION == "matchups" ]]; then
-      mkdir -p ${OUTPUT_FOLDER}/data/matchups
-      FILENAME=${OUTPUT_FOLDER}/data/matchups/${league}_matchups_week${WEEK}
-      if [[ ! -e ${FILENAME}.json ]]; then
-          echo "Getting League $league matchups..."
-          curl  https://api.sleeper.app/v1/league/${league}/matchups/${WEEK} -s > ${FILENAME}.json
+      mkdir -p ${OUTPUT_FOLDER}/data/rosters ${OUTPUT_FOLDER}/data/matchups
+      ROSTERS_FILENAME=${OUTPUT_FOLDER}/data/rosters/${league}_rosters_week${WEEK}.json
+      if [[ ! -e ${ROSTERS_FILENAME} ]]; then
+          curl  https://api.sleeper.app/v1/league/${league}/rosters -s > ${ROSTERS_FILENAME}
       fi
-      echo "Converting League $league matchups"
-      python ./sleeper_parser.py --file ${FILENAME}.json --rosters ${OUTPUT_FOLDER}/data/rosters/${league}_rosters_week$WEEK.json --option matchups --league_id $league --week ${WEEK} >> ${OUTPUT_FOLDER}/data/matchups_week${WEEK}.csv
-      echo "Matchups CSV: ./data/matchups_week${WEEK}.csv"
+      MATCHUPS_FILENAME=${OUTPUT_FOLDER}/data/matchups/${league}_matchups_week${WEEK}.json
+      if [[ ! -e ${MATCHUPS_FILENAME} ]]; then
+          curl  https://api.sleeper.app/v1/league/${league}/matchups/${WEEK} -s > ${MATCHUPS_FILENAME}
+      fi
+      python ./sleeper_parser.py --file ${MATCHUPS_FILENAME} --rosters ${ROSTERS_FILENAME} --option matchups --league_id $league --week ${WEEK}
     fi
-done < "$LEAGUES_FILE"
+}
 
-echo "Done"
+if [[ $OPTION == "users" ]]; then
+    mkdir -p ${OUTPUT_FOLDER}/data/users
+    FILENAME=${OUTPUT_FOLDER}/data/users/${league}_users
+    if [[ ! -e ${FILENAME}.json ]]; then
+        curl  https://api.sleeper.app/v1/league/${league}/users -s > ${FILENAME}.json
+    fi
+    python ./sleeper_parser.py --file ${FILENAME}.json --option users --league_id $league
+else
+  if [[ -n $LEAGUES ]]; then
+    echo "week;league_id;roster_id;owner;matchup_id;points;wins;loses;ties;fpts"
+    for league in $LEAGUES; do
+      process_league $league
+    done
+  else
+    while read league; do
+      process_league $league
+    done < "$LEAGUES_FILE"
+  fi
+fi
 
 exit 0
